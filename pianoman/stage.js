984e73276f8d8341d615f57f451b4dce5a66a3f4
Yennie Song <119934871+yenniesong9@users.noreply.github.com>
2023-12-03 10:23:45 +0900

    Create stage.js
    초안의 초안..입니다. 각 스테이지 이동의 매우 러프한 틀을 잡아놓았습니다.
    
    거의 모든 걸 한 js파일 안에서 처리해서 매우 지저분하고, Class를 적당히 잘 나눠야 할 것 같습니다. (제가 코딩 짱초보라 Class 사용이 매우 미숙합니다. 유진님께서 많이 피드백 주세요ㅜㅜ 미리 죄송하고 감사합니다)
    -------
    stage0. 3인칭 맵에서 player가 돌아다니고, npc 선택하는 과정
    - shift 누르면 다음 스테이지로 이동
    
    stage1. npc 사연 공간 
    - button 누르면 stage2로 이동 (Class Button 생성함)
    
    stage2. rythm game 공간
    - 유진님 코드를 잘 합쳐야 함
    - shift 키 누르면 stage3로 이동
    
    stage3. 성공 / 실패 스크립트 공간
    - 성공 시 / 실패 시 스크립트 다르게 재생되는 로직 필요
    - 일단은 npc 사연 공간과 디스플레이 동일
    - enter 누르면 stage0 (로비)로 이동
    
    ------
    - 현재는 편의를 위해 몇몇 스테이지에서는 key를 눌렀을 때 스테이지 변경으로 짜놓았는데, 마우스로 버튼을 누르는 형태로 바꿀 수 있습니다. 다만 로비에서는 특정 key를 눌러야 다음 스테이지로 넘어가는 게 자연스러워 보입니다.
    
    - 배경 이미지 크기를 windowWidth, windowHeight에 맞게 만들어놨는데, 일관된 플레이 경험을 주려면 배경 이미지 크기가 동일해야 할 것 같다는 생각이 들었습니다.

---
+++ pianoman/stage.js
@@ -0,0 +1,163 @@
+let bg_main;
+let bg_npc;
+
+//스테이지 세기
+let stage = 0;
+
+//player
+let plX = 100;
+let plY = 375;
+let plSpeed = 3;
+
+let isUpKeyPressed = false;
+let isDownKeyPressed = false;
+let isLeftKeyPressed = false;
+let isRightKeyPressed = false;
+
+function preload() {
+  bg_main = loadImage("assets/map_main.png");
+  bg_npc = loadImage("assets/bg_npc.jpg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  bg_main.resize(width, height);
+  bg_npc.resize(width,height);
+  //image(bg_main, 0, 0);
+  //background(0);
+  rectMode(CENTER);
+  textAlign(CENTER);
+}
+
+function draw() {
+    //player 좌표 이동
+    if (isUpKeyPressed) {
+      plY -= plSpeed;
+    }
+    if (isDownKeyPressed) {
+      plY += plSpeed;
+    }
+    if (isLeftKeyPressed) {
+      plX -= plSpeed;
+    }
+    if (isRightKeyPressed) {
+      plX += plSpeed;
+    }
+
+    //스테이지별 
+  if (stage == 0) {
+    lobby();
+  } else if (stage == 1){
+    talk_npc();
+    //스테이지2 이동
+    if(mouseIsPressed && mouseX > width - 110 && mouseX < width-10 &&
+      mouseY > height - 45 && mouseY < height - 15){
+      stage = 2; // 스테이지 2으로 이동
+      }
+  } else if (stage == 2){
+    rhythm();
+    if (keyIsDown(SHIFT)) {
+      stage = 3; //스테이지 3로 이동
+    }
+  } else if (stage == 3){
+    //성공 스크립트와 실패 스크립트 각각 구현해야 함
+    success();  //일단 지금은.. 성공스크립트
+    if (keyIsDown(ENTER)) {
+      stage = 0; // 엔터 누르면 로비로 돌아감
+    }
+  }
+}
+
+function lobby() {
+  background(0);
+  image(bg_main,0,0);
+
+  //npc 위치와 모양 (1. npc.js에 추가해야할 듯 / 2. rect를 각 npc이미지로 대체 필요)
+  fill(200);
+  rect(200,200,30,30);
+
+  if (dist(plX, plY, 200, 200) < 40) { //npc 일정 반경 옆에 갔을 때
+    stroke(200,200,0);
+    strokeWeight(3); // npc 모양에 스트로크
+    rect(200,200,30,30); //각 npc이미지로 대체 필요
+
+  //npc 옆 글씨로 안내
+    noStroke();
+    fill(255);
+    textSize(10);
+    text("press shift", plX, plY + 20);
+
+  //쉬프트 누르면 스테이지 1로 이동
+    if (keyIsDown(SHIFT)){
+      stage = 1;
+      }
+    }
+
+  //player 그리기 (추후 player image로 바꿔야 함)
+  noStroke();
+  fill(150, 0, 170);
+  rect(plX, plY, width/30, width/30);
+
+  //벽 만들기
+  plX = constrain(plX, 125, width);
+  plY = constrain(plY, 150, height);
+  }
+
+function talk_npc(){ //사연공간 세부
+  background(0);
+  image(bg_npc,0,0);
+  fill(0,100);
+  rect(width/2,height/2+200,width,height/2);
+
+  //스크립트 디스플레이 공간
+  fill(255);
+  textSize(20);
+  text("npc 사연 스크립트",width/2,height/2+150);
+
+  //연주하러 가기 버튼
+  let button1 = new Button(width-60, height-30, 100, 30);
+  button1.setTitle("연주하러 가기");
+  button1.show();
+  }
+
+function rhythm(){ // 리듬게임 공간 세부
+  background(0);
+  fill(255);
+  text("리듬게임 영역, press shift to continue",width/2,height/2);
+}
+
+function success(){ //성공 스크립트 세부
+  background(0);
+  image(bg_npc,0,0);
+  fill(0,100);
+  rect(width/2,height/2+200,width,height/2);
+  //자막
+  fill(255);
+  textSize(20);
+  text("성공 스크립트, press enter to go back to lobby",width/2,height/2+150);
+}
+
+//player 방향키에 따른 이동
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    isUpKeyPressed = true;
+  } else if (keyCode === DOWN_ARROW) {
+    isDownKeyPressed = true;
+  } else if (keyCode === LEFT_ARROW) {
+    isLeftKeyPressed = true;
+  } else if (keyCode === RIGHT_ARROW) {
+    isRightKeyPressed = true;
+  }
+}
+
+function keyReleased() {
+  if (keyCode === UP_ARROW) {
+    isUpKeyPressed = false;
+  } else if (keyCode === DOWN_ARROW) {
+    isDownKeyPressed = false;
+  } else if (keyCode === LEFT_ARROW) {
+    isLeftKeyPressed = false;
+  } else if (keyCode === RIGHT_ARROW) {
+    isRightKeyPressed = false;
+  }
+}
